551ff7e7328a22c855ee313563b29fe2be37b7e0
mwasiluk <m.wasiluk89@gmail.com>
2016-06-21 22:21:33 +0200

    improved deepExtend function

--- src/d3-scatterplot-matrix-utils.js
+++ src/d3-scatterplot-matrix-utils.js
@@ -8,23 +8,28 @@ D3ScatterPlotMatrixUtils.prototype.deepExtend = function (out) { //TODO consider
     var emptyOut = {};
 
 
-    if (!out && arguments.length > 1 && arguments[1] instanceof Array) {
+    if (!out && arguments.length > 1 && Array.isArray(arguments[1])) {
         out = [];
     }
     out = out || {};
 
     for (var i = 1; i < arguments.length; i++) {
-        var obj = arguments[i];
-
-        if (!obj)
+        var source = arguments[i];
+        if (!source)
             continue;
 
-        for (var key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                if (typeof obj[key] === 'object')
-                    out[key] = utils.deepExtend(out[key], obj[key]);
-                else
-                    out[key] = obj[key];
+        for (var key in source) {
+            if (!source.hasOwnProperty(key)) {
+                continue;
+            }
+            var isArray = Array.isArray(out[key]);
+            var isObject = utils.isObject(out[key]);
+            var srcObj = utils.isObject(source[key]);
+
+            if (isObject && !isArray && srcObj) {
+                utils.deepExtend(out[key], source[key]);
+            } else {
+                out[key] = source[key];
             }
         }
     }
@@ -62,4 +67,15 @@ D3ScatterPlotMatrixUtils.prototype.inferTraits = function (data, categoryKey, in
         }
     }
     return res
+};
+
+
+D3ScatterPlotMatrixUtils.prototype.isObject = function(a) {
+    return a !== null && typeof a === 'object';
+};
+D3ScatterPlotMatrixUtils.prototype.isNumber = function(a) {
+    return !isNaN(a) && typeof a === 'number';
+};
+D3ScatterPlotMatrixUtils.prototype.isFunction = function(a) {
+    return typeof a === 'function';
 };
